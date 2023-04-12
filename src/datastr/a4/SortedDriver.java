59d14b98e5ab5ddc11758c2dca763e0c4bc151d5
Elaine Philip <emp49872@uga.edu>
2023-04-11 20:24:22 -0400

    selction & merge

--- src/datastr/a4/SortedDriver.java
+++ src/datastr/a4/SortedDriver.java
@@ -8,7 +8,8 @@ public class SortedDriver {
     public static void main(String[] args) {
         try {
 
-            System.out.println("selection-sort (s) merge-sort (m) heap-sort (h) quick-sort-fp (q)");
+            System.out.println("selection-sort (s) merge-sort (m) heap-sort (h) quick-sort-fp (q) "
+            + "quick-sort-rp (r)");
             Scanner scanType = new Scanner(System.in);
 
             File fileName = new File(args[0]);
@@ -35,124 +36,17 @@ public class SortedDriver {
             // user input
 
             if (command.equals("s")) {
-                list.Selection(inputs, count);
+                list.SelectionSort(inputs, count);
                 list.printArr(inputs, count);
-                System.out.println("#Selection-sort comparisons: ");
+                System.out.println("#Selection-sort comparisons: " + list.getNumComp());
+            } else if (command.equals("m")) {
+                list.mergeSort(inputs,  count);
+                list.printArr(inputs, count);
+                System.out.println("#Merge-sort comparisons: " + list.getNumComp());
             } // if
 
 
             System.exit(0);
-
-
-/**
-
-   while (!command.equals("q")) {
-                boolean invalid = false;
-                Scanner scanCom  = new Scanner(System.in);
-                command = scanCom.next();
-                // user input
-
-                if (command.equals("p")) {
-                    if (isInt) {
-                        listI.inOrder();
-                    } else if (isDouble) {
-                        listD.inOrder();
-                    } else if (isString) {
-                        listS.inOrder();
-                    } // if
-                    // prints list
-                } else if (command.equals("i")) {
-                    if (isInt) {
-                        listI.inOrder();
-                        System.out.print("Enter a number to insert: ");
-                        scanCom = new Scanner(System.in);
-                        int val = scanCom.nextInt();
-                        listI.insert(val);
-                        listI.inOrder();
-                    } else if (isDouble) {
-                        listD.inOrder();
-                        System.out.print("Enter a number to insert: ");
-                        scanCom = new Scanner(System.in);
-                        double val = scanCom.nextDouble();
-                        listD.insert(val);
-                        listD.inOrder();
-                    } else if (isString) {
-                        listS.inOrder();
-                        System.out.print("Enter a string to insert: ");
-                        scanCom = new Scanner(System.in);
-                        String val = scanCom.next();
-                        listS.insert(val);
-                        listS.inOrder();
-                    } // if
-                    // prints in-order tree, inserts item, and prints new tree
-                } else if (command.equals("r")) {
-                    if (isInt) {
-                        listI.inOrder();
-                        System.out.print("Enter a number to search: ");
-                        scanCom = new Scanner(System.in);
-                        int val = scanCom.nextInt();
-                        listI.retrieve(val);
-                    } else if (isDouble) {
-                        listD.inOrder();
-                        System.out.print("Enter a number to search: ");
-                        scanCom = new Scanner(System.in);
-                        double val = scanCom.nextDouble();
-                        listD.retrieve(val);
-                     } else if (isString) {
-                        listS.inOrder();
-                        System.out.print("Enter a string to search: ");
-                        scanCom = new Scanner(System.in);
-                        String val = scanCom.next();
-                        listS.retrieve(val);
-                     } // if
-              } else if (command.equals("c")) {
-                    if (isInt) {
-                        listI.inOrder();
-                        System.out.print("Enter a number: ");
-                        scanCom = new Scanner(System.in);
-                        int val = scanCom.nextInt();
-                        System.out.print(val + " cousins: ");
-                        listI.getCousins(val);
-                    } else if (isDouble) {
-                        listD.inOrder();
-                        System.out.print("Enter a number: ");
-                        scanCom = new Scanner(System.in);
-                        double val = scanCom.nextDouble();
-                        System.out.print(val + " cousins: ");
-                        listD.getCousins(val);
-                    } else if (isString) {
-                        listS.inOrder();
-                        System.out.print("Enter a string: ");
-                        scanCom = new Scanner(System.in);
-                        String val = scanCom.next();
-                        System.out.print(val + " cousins: ");
-                        listS.getCousins(val);
-                    } // if
-                    // prints original list, deletes item, and prints new list
-
-
-
-
-               } else if (command.equals("q")) {
-                    System.out.println("Exiting the program...");
-                    System.exit(0);
-                } else {
-                    invalid = true;
-                } // if
-                // all command cases
-
-                if (invalid) {
-                    System.out.print("Invalid command, try again: ");
-                } else if (!command.equals("q")){
-                    System.out.print("Enter a command: ");
-                } // if
-                //prompting user
-
-            } // while
-            // loop to prompt user
-
-            */
-
 
         } catch (FileNotFoundException fnfe) {
             System.out.println("File Not Found");
--- src/datastr/a4/Sorting.java
+++ src/datastr/a4/Sorting.java
@@ -8,7 +8,8 @@ public class Sorting {
     } // default constructor
 
 
-    public void Selection(int[] inputs, int length) {
+    public void SelectionSort(int[] inputs, int length) {
+        numComp = 0;
         int endIndex = length - 1;
         for (int i = 0; i < endIndex; i++) {
             swap(inputs, i, minIndex(inputs, i, endIndex));
@@ -26,12 +27,66 @@ public class Sorting {
 
         for (int i = start + 1; i <= end; i++) {
             if (values[i] < values[indexOfMin]) {
-                    indexOfMin = i;
+                indexOfMin = i;
+                numComp++;
+            } else {
+                numComp++;
             } // if
         } // for
 
         return indexOfMin;
     } // minIndex
+
+
+    public void mergeSort(int[] inputs, int length) {
+        numComp = 0;
+        if (length < 2) {
+            return;
+        } // if
+        // if only one element
+
+        int mid = length / 2; // midpoint
+        int[] left = new int[mid];
+        int[] right = new int[length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = inputs[i];
+        } // for
+        for (int i = mid; i < length; i++) {
+            right[i - mid] = inputs[i];
+        } // for
+        // splits into two subarrays
+
+        mergeSort(left, mid);
+        mergeSort(right, length - mid);
+
+        merge(inputs, left, right, mid, length - mid);
+    } // mergeSort
+
+    public  void merge (int[] inputs, int[] l, int[] r, int left, int right) {
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                inputs[k++] = l[i++];
+            } else {
+                inputs[k++] = r[j++];
+            } // if
+            numComp++;
+        } // while
+
+        while (i < left) {
+            inputs[k++] = l[i++];
+        } // while
+
+        while (j < right) {
+            inputs[k++] = r[j++];
+        } // while
+    } // merge
+
 
 
     public void printArr(int[] arr, int count) {
@@ -41,5 +96,11 @@ public class Sorting {
         // prints vals of array
         System.out.println();
     } // printArr
+
+    public int getNumComp() {
+        return numComp;
+    } // getNumComp
+
+
 
 } // Sorting Class
